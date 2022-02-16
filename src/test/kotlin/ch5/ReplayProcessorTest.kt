61da2bbc0f6a8169bc369f72c87f2bf5f85ef7f2
Haf <haf@protonmail.ch>
2022-02-16 22:07:50 +0100

    using sinks.many, looks ugly

--- src/test/kotlin/ch5/ReplayProcessorTest.kt
+++ src/test/kotlin/ch5/ReplayProcessorTest.kt
@@ -4,25 +4,23 @@ import org.junit.jupiter.api.Test
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 import reactor.core.publisher.ReplayProcessor
+import reactor.core.publisher.Sinks
 import reactor.test.StepVerifier
 
-// TODO: Use Sinks.many
 class ReplayProcessorTest {
 
     @Test
     fun replayProcessor() {
-        val processor = ReplayProcessor.create<String>(
-            2, false
-        )
-        produce(processor.sink())
-        consume(processor)
+        val sink = Sinks.many().replay().limit<String>(2)
+        produce(sink)
+        consume(sink.asFlux())
     }
 
-    private fun produce(sink: FluxSink<String>) {
-        sink.next("1")
-        sink.next("2")
-        sink.next("3")
-        sink.complete()
+    private fun produce(sink: Sinks.Many<String>) {
+        sink.emitNext("1", Sinks.EmitFailureHandler.FAIL_FAST)
+        sink.emitNext("2", Sinks.EmitFailureHandler.FAIL_FAST)
+        sink.emitNext("3", Sinks.EmitFailureHandler.FAIL_FAST)
+        sink.emitComplete(Sinks.EmitFailureHandler.FAIL_FAST)
     }
 
     private fun consume(publisher: Flux<String>) {
